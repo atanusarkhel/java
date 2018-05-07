1c64d8c7284e633472a3e0e0e45b9f52cb6a111e
Atanu Sarkhel <37656802+atanusarkhel@users.noreply.github.com>
2018-05-07 14:47:17 +0530

    Update AnonymousDemo.java

--- AnonymousDemo.java
+++ AnonymousDemo.java
@@ -1,1 +1,20 @@
+interface Student
+{
+  public static void print()
+  {
+    System.out.println("Interface print method");
+  }
+  public default write()
+  {
+    System.out.println("Interface print method");
+  }
+  public abstract void show();
+}
 
+class InterfaceAnonymousDemo
+{
+  public static void main(String Args[])
+  {
+    Student st=new student(){public void show(){System.out.println("main class show method");}};
+  }
+}
